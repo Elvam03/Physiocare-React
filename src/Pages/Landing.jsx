fcef39950a911d3ba0667f119ccb025f318857d7
Elvam03 <velmakemunto03@gmail.com>
2025-01-18 15:32:43 +0300

    final

--- src/Pages/Landing.jsx
+++ src/Pages/Landing.jsx
@@ -131,12 +131,7 @@ const Landing = () => {
             <main id="home">
                 <div
                     className="h-screen bg-fixed bg-center bg-cover"
-                    style={{
-                        backgroundImage: "url('/Images/four.jpg')",
-                        backgroundAttachment: "fixed",
-                        backgroundPosition: "center",
-                        backgroundSize: "cover",
-                    }}
+                    style={{ backgroundImage: "url('/Images/landing img.jpg')" }}
                 >
                     <div className="flex items-center justify-center h-full bg-black bg-opacity-50">
                         <div className="text-center">
